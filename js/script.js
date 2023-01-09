eb7ac35321ee7887f2622838d20d8ea9e7ef4547
Lukasz Szmytka <szmytka983@gmail.com>
2023-01-09 19:14:22 +0100

    Changed currency rates, rateElement.innerText fixed

--- js/script.js
+++ js/script.js
@@ -1,42 +1,36 @@
 {
-  const calculateRate = (amount, currency) => {
-    const EUR = 4.7804;
-    const USD = 4.9;
-    const CHF = 4.852;
-    const HRK = 0.6345;
-    const TRY = 0.2635;
-    const GBP = 5.4537;
+  const calculateResult = (amount, currency) => {
+    const EUR = 4.697;
+    const USD = 4.401;
+    const CHF = 4.7606;
+    const HRK = 0.6233;
+    const TRY = 0.2344;
+    const GBP = 5.3515;
 
     switch (currency) {
       case "EUR":
-        rate = EUR;
         return amount / EUR;
 
       case "USD":
-        rate = USD;
         return amount / USD;
 
       case "CHF":
-        rate = CHF;
         return amount / CHF;
 
       case "HRK":
-        rate = HRK;
         return amount / HRK;
 
       case "TRY":
-        rate = TRY;
         return amount / TRY;
 
       case "GBP":
-        rate = GBP;
         return amount / GBP;
     }
   };
 
-  const updateSumText = (sum, currency) => {
-    const sumElement = document.querySelector(".js-sum");
-    sumElement.innerHTML = `${sum.toFixed(2)} ${currency} `;
+  const updateResultText = (result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerHTML = `${result.toFixed(2)} ${currency} `;
   };
 
   const onFormSubmit = (event) => {
@@ -45,15 +39,15 @@
     const amountElement = document.querySelector(".js-amount");
 
     const currencyElement = document.querySelector(".js-currency");
-    const rateElement = document.querySelector(".js-rate");
 
     const amount = amountElement.value;
     const currency = currencyElement.value;
-    const sum = calculateRate(amount, currency);
+    const result = calculateResult(amount, currency);
+    const rateElement = document.querySelector(".js-rate");
 
-    rateElement.innerText = rate;
+    rateElement.innerText = (amount / result).toFixed(2);
 
-    updateSumText(sum, currency);
+    updateResultText(result, currency);
   };
 
   const init = () => {
